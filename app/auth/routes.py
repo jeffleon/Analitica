92237c3bce8eabc3293b44dd5030340e3f8f799e
Jhonatan Rodriguez <rodriguezjhonatanalexander@gmail.com>
2021-02-12 16:12:31 -0500

    Se agrega autenticacion con jwt desde laravel

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,11 @@
 from . import auth
-from flask import render_template, session, redirect, url_for, flash
-
+from flask import render_template, session, redirect, url_for, flash, request
+import jwt
 
 @auth.route('/auth')
 def index():
-    return render_template('auth.html')
+    bearer = request.headers.get("Authorization").split()[1]
+    try:
+        return jwt.decode(bearer, "0YsAxFFsjbR6O1pORbNDmpHbq4zwWoJtbhIyCYYKGylL3o6knuUF0FXTVDpG80Dz", algorithms=["HS256"])
+    except: 
+        return redirect(url_for('auth.index'))
